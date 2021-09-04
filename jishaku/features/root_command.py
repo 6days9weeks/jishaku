0c1ea962f61b83c20a03e12463541b88e14c9467
6days9weeks <6days9weeks@pm.me>
2021-09-04 16:23:37 +0900

    aaaa

--- jishaku/features/root_command.py
+++ jishaku/features/root_command.py
@@ -63,7 +63,7 @@ class RootCommand(Feature):
         """
 
         summary = [
-            f"Jishaku v{package_version('jishaku')}, discord.py `{package_version('discord.py')}`, "
+            f"Jishaku v{package_version('jishaku')}, Novus `{package_version('novus')}`, VoxelBotUtils `{package_version('voxelbotutils')}` "
             f"`Python {sys.version}` on `{sys.platform}`".replace("\n", ""),
             f"Module was loaded <t:{self.load_time.timestamp():.0f}:R>, "
             f"cog was loaded <t:{self.start_time.timestamp():.0f}:R>.",
